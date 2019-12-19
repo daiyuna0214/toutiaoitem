3827a7d2bbfc958027426fcc375b0318465e764d
daiyuna0214 <2034892606@qq.com>
2019-12-19 22:19:02 +0800

    15,主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -3,27 +3,29 @@
       <div class="title">
           <img src="../../assets/img/logo_admin.png" alt="">
       </div>
-      <el-menu style="width:200px" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
-         <el-menu-item>
+      <!-- elementUI中使用router开启路由模式 -->
+      <el-menu router style="width:200px" background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b">
+         <!-- index作为path跳转地址，相当于之前router-link里面的to属性 -->
+         <el-menu-item index='/home'>
            <span slot="title">首页</span>
          </el-menu-item>
-          <el-submenu>
+          <el-submenu index='1'>
             <template slot="title">
                <span>内容管理</span>
             </template>
-          <el-menu-item>发布文章</el-menu-item>
-          <el-menu-item>内容列表</el-menu-item>
-          <el-menu-item>评论列表</el-menu-item>
-          <el-menu-item>素材管理</el-menu-item>
+          <el-menu-item index='/home/publish'>发布文章</el-menu-item>
+          <el-menu-item index='/home/articles'>内容列表</el-menu-item>
+          <el-menu-item index='/home/comment'>评论列表</el-menu-item>
+          <el-menu-item index='/home/material'>素材管理</el-menu-item>
       </el-submenu>
-          <el-submenu>
+          <el-submenu index='2'>
             <template slot="title">
                <span>粉丝管理</span>
             </template>
-          <el-menu-item>图文数据</el-menu-item>
-          <el-menu-item>粉丝概况</el-menu-item>
-          <el-menu-item>粉丝画像</el-menu-item>
-          <el-menu-item>粉丝列表</el-menu-item>
+          <el-menu-item index='/home/picture'>图文数据</el-menu-item>
+          <el-menu-item index='/home/fansinfo'>粉丝画像</el-menu-item>
+          <el-menu-item index='/home/fanslife'>粉丝概况</el-menu-item>
+          <el-menu-item index='/home/fanslist'>粉丝列表</el-menu-item>
       </el-submenu>
          <el-menu-item>
            <span slot="title">账户信息</span>
