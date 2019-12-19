7266e07de2cb85a3c406b1e28f011d9b67e53f4a
daiyuna0214 <2034892606@qq.com>
2019-12-19 16:22:31 +0800

    9,主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>我是主页</div>
+  <div>
+    <el-container>
+      <!-- 左右布局 -->
+        <el-aside width="200px">左侧</el-aside>
+        <el-container>
+             <el-header>Header</el-header>
+             <el-main>Main</el-main>
+        </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
