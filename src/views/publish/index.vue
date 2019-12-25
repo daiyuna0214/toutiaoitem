51a508a71eed45c214c3f30cd599c20ee2a5e6a1
Your Name <you@example.com>
2019-12-25 18:37:38 +0800

    39,发表文章-发布文章检验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,12 +3,12 @@
       <bread-crumb slot="header">
           <template slot="title">发布文章</template>
       </bread-crumb>
-      <el-form style="margin-left:50px" label-width="100px">
-          <el-form-item label="标题">
-              <el-input placeholder="文章名称" style="width:40%"></el-input>
+      <el-form style="margin-left:50px" label-width="100px" :model="formData" :rules="publishRules" ref="publishForm">
+          <el-form-item label="标题" prop="title">
+              <el-input placeholder="文章名称" style="width:40%" v-model="formData.title"></el-input>
           </el-form-item>
-          <el-form-item label="内容">
-              <el-input type="textarea"></el-input>
+          <el-form-item label="内容" prop="content">
+              <el-input type="textarea" v-model="formData.content"></el-input>
           </el-form-item>
           <el-form-item label="封面">
                <el-radio-group>
@@ -18,14 +18,14 @@
                  <el-radio :label="9">自动</el-radio>
                </el-radio-group>
           </el-form-item>
-          <el-form-item label="频道">
-               <el-select v-model="channels.id">
+          <el-form-item label="频道" prop="channel_id">
+               <el-select v-model="formData.channel_id">
                   <el-option v-for="item in channels" :key="item.id" :value="item.id" :label="item.name"></el-option>
                </el-select>
           </el-form-item>
           <el-form-item>
-               <el-button type="primary">发布</el-button>
-               <el-button>存入草稿</el-button>
+               <el-button type="primary" @click="publishArticles">发布</el-button>
+               <el-button @click="publishArticles">存入草稿</el-button>
           </el-form-item>
       </el-form>
   </el-card>
@@ -35,7 +35,24 @@
 export default {
   data () {
     return {
-      channels: []// 定义一个数组接收文章频道
+      channels: [], // 定义一个数组接收文章频道
+      //   定义一个表单数据
+      formData: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: {// 封面
+          type: '',
+          inages: ''
+        },
+        channel_id: null// 频道
+      },
+      //   定义规则
+      publishRules: {
+        title: [{ required: true, message: '标题不能为空' },
+          { min: 5, max: 30, message: '文章标题长度需要在5-30字之间' }],
+        content: [{ required: true, message: '文章内容不能为空' }],
+        channel_id: [{ required: true, message: '频道分类不能为空' }]
+      }
     }
   },
   methods: {
@@ -46,6 +63,16 @@ export default {
       }).then(res => {
         // console.log(res)
         this.channels = res.data.channels
+      })
+    },
+    // 发布文章
+    publishArticles () {
+      // 自动校验
+      this.$refs.publishForm.validate(function (value) {
+        if (value) {
+          alert('校验成功')
+        //   调接口发请求
+        }
       })
     }
   },
