7fb85445cdbd89746d585ab35da411ac8f399692
Your Name <you@example.com>
2019-12-27 21:26:50 +0800

    50,账户信息-数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,19 +5,19 @@
       </bread-crumb>
       <el-form style="margin-left:30px" label-width="100px" >
           <el-upload class='head-upload' action="" :show-file-list="false">
-              <img src="../../assets/img/collect.png" alt="">
+              <img :src="formData.photo?formData.photo:defaultImg" alt="">
           </el-upload>
           <el-form-item label="用户名">
-              <el-input style="width:30%"></el-input>
+              <el-input v-model="formData.name" style="width:30%"></el-input>
           </el-form-item>
           <el-form-item label="简介">
-              <el-input style="width:30%"></el-input>
+              <el-input style="width:30%" v-model="formData.intro"></el-input>
           </el-form-item>
           <el-form-item label="邮箱">
-              <el-input style="width:30%"></el-input>
+              <el-input style="width:30%" v-model="formData.email"></el-input>
           </el-form-item>
           <el-form-item label="手机号">
-              <el-input style="width:30%"></el-input>
+              <el-input style="width:30%" v-model="formData.mobile"></el-input>
           </el-form-item>
           <el-form-item>
               <el-button type="primary">保存信息</el-button>
@@ -28,7 +28,34 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 定义用户信息数据
+      formData: {
+        name: '',
+        intro: '',
+        photo: '',
+        email: '',
+        mobile: ''
+      },
+      defaultImg: require('../../assets/img/collect.png')
+    }
+  },
+  methods: {
+    // 获取用户个人信息
+    getUserMessage () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(res => {
+        // console.log(res)
+        this.formData = res.data
+      })
+    }
+  },
+  created () {
+    // 页面创建完成获取信息
+    this.getUserMessage()
+  }
 }
 </script>
 
