97dcf3f99261565e207831a258f5264caaa1d7e0
Your Name <you@example.com>
2019-12-27 21:52:57 +0800

    51,账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,24 +3,25 @@
       <bread-crumb slot="header">
          <template slot="title">账户信息</template>
       </bread-crumb>
-      <el-form style="margin-left:30px" label-width="100px" >
+      <el-form style="margin-left:30px" label-width="100px"
+      ref='myForm' :model="formData" :rules="userRules">
           <el-upload class='head-upload' action="" :show-file-list="false">
               <img :src="formData.photo?formData.photo:defaultImg" alt="">
           </el-upload>
-          <el-form-item label="用户名">
+          <el-form-item label="用户名" prop="name">
               <el-input v-model="formData.name" style="width:30%"></el-input>
           </el-form-item>
-          <el-form-item label="简介">
+          <el-form-item label="简介" prop="intro">
               <el-input style="width:30%" v-model="formData.intro"></el-input>
           </el-form-item>
-          <el-form-item label="邮箱">
+          <el-form-item label="邮箱" prop="email">
               <el-input style="width:30%" v-model="formData.email"></el-input>
           </el-form-item>
           <el-form-item label="手机号">
-              <el-input style="width:30%" v-model="formData.mobile"></el-input>
+              <el-input style="width:30%" v-model="formData.mobile" disabled></el-input>
           </el-form-item>
           <el-form-item>
-              <el-button type="primary">保存信息</el-button>
+              <el-button type="primary" @click="saveUserMessage">保存信息</el-button>
           </el-form-item>
       </el-form>
   </el-card>
@@ -38,7 +39,18 @@ export default {
         email: '',
         mobile: ''
       },
-      defaultImg: require('../../assets/img/collect.png')
+      defaultImg: require('../../assets/img/collect.png'),
+      userRules: {
+        name: [{ required: true, message: '用户名不能为空' },
+          { min: 1, max: 8, message: '用户名在1-7之间' }],
+        intro: [{ required: true, message: '简介不能为空' }],
+        email: [{ required: true, message: '邮箱不能为空' },
+          {
+            pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
+            message: '邮箱格式不正确'
+          }],
+        mobile: ''
+      }
     }
   },
   methods: {
@@ -49,6 +61,24 @@ export default {
       }).then(res => {
         // console.log(res)
         this.formData = res.data
+      })
+    },
+    saveUserMessage () {
+      this.$refs.myForm.validate((value) => {
+        if (value) {
+          // 调接口发请求
+          this.$axios({
+            url: '/user/profile',
+            method: 'patch',
+            data: this.formData
+          }).then(res => {
+            // 保存成功
+            this.$message({
+              type: 'success',
+              message: '保存成功'
+            })
+          })
+        }
       })
     }
   },
