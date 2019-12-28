e5d266522eee3329c799c02632bd430169c0123b
daiyuna0214 <2034892606@qq.com>
2019-12-28 17:40:53 +0800

    55,echarts在项目中的应用及页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,11 +1,17 @@
 // 引入路由
 import router from '../router'
+// 引入页面进度条
+import nprogress from 'nprogress'
+// 引入进度条样式
+import 'nprogress/nprogress.css'
 // 全局前置守卫
 router.beforeEach(function (to, from, next) {
   // to即将要进入的目标
   // from当前导航正要离开的路由
 //   console.log(to)
   if (to.path.startsWith('/home')) {
+    // 开启进度条
+    nprogress.start()
     let token = localStorage.getItem('user-token')
     if (token) {
       next()
@@ -17,4 +23,9 @@ router.beforeEach(function (to, from, next) {
     // 如果是登录页直接放行
     next()
   }
+})
+// 全局后置守卫
+router.afterEach(function () {
+  // 关闭进度条
+  nprogress.done()
 })
